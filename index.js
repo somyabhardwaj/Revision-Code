b0d6e2725eeb54bb1a2f1139d228ee5d89a1d025
Somya Bhardwaj <somyabhardwaj004@gmail.com>
2023-09-04 12:48:25 +0530

    8th Array Methods

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-let b = "hello my name is somya";
-let c = 0;
+// let b = "hello my name is somya";
+// let c = 0;
 
-// setTimeout(() => {
-//   function displayNextCharacter() {
-//     if (c < b.length) {
-//       document.getElementById("a").innerHTML += b.charAt(c);
-//       c++;
+// // setTimeout(() => {
+// //   function displayNextCharacter() {
+// //     if (c < b.length) {
+// //       document.getElementById("a").innerHTML += b.charAt(c);
+// //       c++;
       
-//       setTimeout(displayNextCharacter, 500); // Adjust the delay (in milliseconds) as needed
-//     }
-//   }
+// //       setTimeout(displayNextCharacter, 500); // Adjust the delay (in milliseconds) as needed
+// //     }
+// //   }
 
-//   displayNextCharacter();
-// }, 2000);
+// //   displayNextCharacter();
+// // }, 2000);
 
-// document.getElementById("a").innerHTML += b.split(" ",3);
+// // document.getElementById("a").innerHTML += b.split(" ",3);
 
-// document.getElementById("a").innerHTML +=b.slice(5,20);
+// // document.getElementById("a").innerHTML +=b.slice(5,20);
 
-// it returns index value
-// document.getElementById("a").innerHTML += b.search("s");
+// // it returns index value
+// // document.getElementById("a").innerHTML += b.search("s");
 
-function greet (){
-    let hello = "Hello how Are You ??"
-    return hello;
+// function greet (){
+//     let hello = "Hello how Are You ??"
+//     return hello;
+// }
+// let d= greet();
+// let Arr= ["hello" ,32 , d]
+// Arr.push("New");
+// Arr.unshift("Somya");
+// Arr.pop();
+// Arr.shift();
+// Arr[3]="My name is somya";
+// document.getElementById("hello").innerHTML += Arr[3];
+// document.getElementById("a").innerHTML += Arr.length;
+
+// Array Operations
+
+// let Arr1 = ["hello", "I", "Am", "Somya"];
+// let Arr2 = ["I", "Am", "from", "Kanpur"];
+
+// let Arr3 = Arr1.concat(Arr2);
+
+// document.getElementById("hello").innerHTML += Arr3.indexOf("from");
+
+// document.getElementById("hello").innerHTML += `${Arr3}`;
+
+let Arr1 =[20,30,25,26,10,15,14,19,13,12,12,15,14,10,56,5,44]
+function Adults(Arr1){
+    return Arr1 >=18;
 }
-let d= greet();
-let Arr= ["hello" ,32 , d]
-document.getElementById("hello").innerHTML += Arr[2];
+
+document.getElementById("hello").innerHTML += Arr1.filter(Adults).length;
+
+
+
+
