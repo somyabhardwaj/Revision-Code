69db43e8f10fc5fd971e152e66567db1c133ae84
Somya Bhardwaj <somyabhardwaj004@gmail.com>
2023-09-14 11:49:51 +0530

    Dom prac

--- Dom.js
+++ Dom.js
@@ -1,11 +1,21 @@
 
 
 
-document.getElementById("myBtn").addEventListener("click", function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-  });
+// document.getElementById("myBtn").addEventListener("click", function myFunction() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
   
-  
+  function myFunction (){
+
+  const hello = document.createElement("h1");
+
+  const textnode = document.createTextNode("water");
+
+  hello.appendChild(textnode);
+
+  document.getElementById("myList").appendChild(hello);
+
+  }
   
   
   
