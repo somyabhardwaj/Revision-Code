c2ea2a555be1df77501857dbe67b0615c7b39a4b
Somya Bhardwaj <somyabhardwaj004@gmail.com>
2023-09-14 11:59:59 +0530

    Dom

--- Dom.js
+++ Dom.js
@@ -5,17 +5,29 @@
 //     document.getElementById("demo").innerHTML = "Hello World";
 //   });
   
-  function myFunction (){
+  // function myFunction (){
 
-  const hello = document.createElement("h1");
+  // const hello = document.createElement("h1");
 
-  const textnode = document.createTextNode("water");
+  // const textnode = document.createTextNode("water");
 
-  hello.appendChild(textnode);
+  // hello.appendChild(textnode);
 
-  document.getElementById("myList").appendChild(hello);
+  // document.getElementById("myList").appendChild(hello);
 
-  }
+  // }
+
+  const  nodeMap = document.getElementById("myImg").attributes;
+
+  let text ="";
+
+  for (let i=0;i<nodeMap.length;i++)
+{
+
+  text += nodeMap[i].name + " = " + nodeMap[i].value + "<br>"
+
+  document.getElementById("demo").innerHTML = text;
+}
   
   
   
